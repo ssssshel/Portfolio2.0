a4aa2174a0d8b6889021b3f1ce6f97f8171bda32
ssssshel <angelarteaga989@gmail.com>
2022-07-07 15:06:59 -0500

    ff

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image"
+import { useRef } from "react"
 
-export default function ProjectItem({ name, description, tools, details, img, url, repo }: { name: string, description: string, tools: Array<string>, details: string, img: string, url: string, repo: string }) {
+export default function ProjectItem({ name, description, tools, details, img, url, repo }: { name: string, description: string, tools: Array<string>, details: string, img: string, url: string, repo: string }): JSX.Element {
+
+  const ref = useRef()
+
+
+  function showDetails() {
+    ref.current!.
+  }
 
   return (
-    <div className='relative h-60% w-full drop-shadow-lg'>
-      <Image alt='' src={'/img/projects/dione.png'} layout='fill' objectFit="cover" objectPosition="center" />
-      <div className='absolute flex flex-col justify-center w-full h-full gap-4 p-10 bg-dark/70'>
-        <p className='text-3xl text-light'>Dione</p>
-        <p className='text-light'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit maxime itaque facilis perferendis. Deleniti necessitatibus ducimus placeat unde, eligendi perferendis earum, molestiae autem, debitis ratione veritatis quae? Ex, repellat magnam.</p>
-        <button className='bg-medium hover:bg-light '><p>Ver más</p></button>
+    <div onMouseOver={ } className='relative h-60% w-full drop-shadow-lg'>
+      <Image alt='' src={`/img/projects/${img}`} layout='fill' objectFit="cover" objectPosition="center" />
+      <div ref={ref} className='absolute flex flex-col justify-center w-full h-full gap-4 p-10 bg-dark/70'>
+        <p className='text-3xl text-light'>{name}</p>
+        <p className='text-light'>{description}</p>
+        <button className='bg-medium hover:bg-light' onClick={() => { }} ><p>Ver más</p></button>
       </div>
     </div>
   )
